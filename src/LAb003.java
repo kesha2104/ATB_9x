7ecbe60e1076b6e330d6b38ade0f0fd601f229e2
Kesha004 <kesha.sangani@gmail.com>
2024-11-10 12:49:27 +0530

    all the files added

---
+++ src/LAb003.java
@@ -0,0 +1,5 @@
+public class LAb003 {
+    public static void main(String[] args) {
+        System.out.println("lab003");
+    }
+}
---
+++ src/oct/exe_0811/Lab001.java
@@ -0,0 +1,8 @@
+package oct.exe_0811;
+
+public class Lab001 {
+    public static void main(String[] args) {
+    System.out.print("hello");
+
+    }
+}
